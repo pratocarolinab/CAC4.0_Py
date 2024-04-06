e8788f33abb85593d31a467c1e27ee250d148686
escrib <sebascraton@gmail.com.com>
2024-04-06 10:33:55 -0300

    btn-top

---
+++ js/btn-top.js
@@ -0,0 +1,13 @@
+const goTop = (() => {
+  let button = document.getElementById("goTop");
+  window.onscroll = () => {
+    button.classList[
+        (document.documentElement.scrollTop > 200) ? "add" : "remove"
+      ]("visible")
+  }
+  // button.onclick = () => {
+  //   window.scrollTo({
+  //     top:0, behavior:"smooth"
+  //   })
+  // }
+})();
