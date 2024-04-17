3fd5a07409598bd244adc1fb205d8bb62ea2157e
escrib <sebascraton@gmail.com.com>
2024-04-17 19:17:13 -0300

    header/efect-shadow/efect-mininimize

--- js/btn-top.js
+++ js/btn-top.js
@@ -1,9 +1,13 @@
 const goTop = (() => {
   let button = document.getElementById("goTop");
+  let header_shadow = document.getElementsByTagName("header");
   window.onscroll = () => {
     button.classList[
         (document.documentElement.scrollTop > 300) ? "add" : "remove"
-      ]("visible")
+      ]("visible");
+    header_shadow[0].classList[
+        (document.documentElement.scrollTop > 50) ? "add" : "remove"
+      ]("header_No-shadow_Mini");
   }
   button.onclick = () => {
     window.scrollTo({
