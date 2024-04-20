d035c25fd7d62a0b92edc2c68b9f7979e571e15f
escrib <sebascraton@gmail.com.com>
2024-04-20 17:40:16 -0300

    section-clients-mobile

--- js/user-recommend.js
+++ js/user-recommend.js
@@ -1,7 +1,6 @@
 fetch("https://randomuser.me/api/?page=3&results=3&seed=ttttttaa&nat=es&inc=gender,name,dob,picture&noinfo")
     .then((response) => response.json())
     .then((data) => {
-        //  console.log(data);
         for (let i = 0; i < 4; i++) {
             const user = data.results[i];
             const name = user.name.first + " " + user.name.last;
@@ -13,5 +12,3 @@ fetch("https://randomuser.me/api/?page=3&results=3&seed=ttttttaa&nat=es&inc=gend
             const element_name = case_item_name[i].innerHTML = name;
         }
     });
-
-    // page=3&results=10&seed=abc
