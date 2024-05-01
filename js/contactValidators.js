73bbf2ebcba1fd4a3474e61256204b994248ee0b
mrt <siddeisland@gmail.com>
2024-05-01 10:08:50 -0300

    .

--- js/contactValidators.js
+++ js/contactValidators.js
@@ -3,12 +3,14 @@ const contactValidators = () => {
     const name = document.querySelector(".name");
     const completeName = document.querySelector("#completeName");
     completeName.addEventListener('input', (event) => {
-            let valueInput = event.target.value
-            if (valueInput.length < 3 || valueInput.length > 40) {
-                name.style.display = 'block';
-            }else if(valueInput.length > 3 && valueInput.length <=40)
-            name.style.display = 'none'; 
-        });
+        let valueInput = event.target.value
+        if (valueInput.length == 0) {
+            name.style.display = 'none';
+        } else if (valueInput.length < 3 || valueInput.length > 40) {
+            name.style.display = 'block';
+        } else if (valueInput.length > 3 && valueInput.length <= 40)
+            name.style.display = 'none';
+    });
 }
 
 
