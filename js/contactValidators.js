b208682fbb6738ce0ada09fb388db8c94191eda9
mrt <siddeisland@gmail.com>
2024-05-01 10:45:28 -0300

    emailvalidator

--- js/contactValidators.js
+++ js/contactValidators.js
@@ -13,5 +13,27 @@ const contactValidators = () => {
     });
 }
 
+const emailValidators = () => {
+    const email = document.getElementById("email");
+    const errEmail = document.querySelector('.email');
+    email.addEventListener('input', (event) => {
+        valueEmail = event.target.value
+        const validatorsEmail  = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+        console.log(validatorsEmail.test(valueEmail))
+        if(!validatorsEmail.test(valueEmail)) {
+            errEmail.style.display = 'block';
+        } else {
+            errEmail.style.display = 'none';
+        } 
+    });
+   
+}
+
+const sendForm = (event) => {
+     event.preventDefault();
+     
+}
+
 
 contactValidators();
+emailValidators();
